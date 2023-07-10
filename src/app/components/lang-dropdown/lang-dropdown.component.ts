32b19e53f44ba2d127fd28c944c040afaeebfd80
Riccardo Campani <riccardocampani96@gmail.com>
2023-07-10 18:55:18 +0200

    docs(lang-dropdown): add documentation for the lang-dropdown.component.ts

--- src/app/components/lang-dropdown/lang-dropdown.component.ts
+++ src/app/components/lang-dropdown/lang-dropdown.component.ts
@@ -1,8 +1,12 @@
-import {Component} from '@angular/core';
-import {environment} from "../../../environments/environment";
-import {TranslateService} from "@ngx-translate/core";
-import {DropdownModule} from "primeng/dropdown";
-import {FormsModule} from "@angular/forms";
+/**
+ * LangDropdownComponent is an Angular component that represents a language dropdown.
+ * It allows the user to select a language and triggers a translation when the language is changed.
+ */
+import { Component } from '@angular/core';
+import { environment } from "../../../environments/environment";
+import { TranslateService } from "@ngx-translate/core";
+import { DropdownModule } from "primeng/dropdown";
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-lang-dropdown',
@@ -13,15 +17,31 @@ import {FormsModule} from "@angular/forms";
 })
 export class LangDropdownComponent {
 
+  /**
+   * langOptions represents the available language options.
+   * It is populated from the environment configuration.
+   */
   readonly langOptions = environment.langOptions;
 
+  /**
+   * lang represents the currently selected language.
+   * It is initialized with the first option from langOptions.
+   */
   lang: string = this.langOptions[0];
 
+  /**
+   * Creates an instance of LangDropdownComponent.
+   * @param translateService The TranslateService used for language translation.
+   */
   constructor(
     private translateService: TranslateService
   ) {
   }
 
+  /**
+   * Translates the application to the selected language.
+   * It calls the TranslateService's use() method with the selected language.
+   */
   translate(): void {
     this.translateService.use(this.lang);
   }
