3d605fb2081a3717ba622949df57ab8490dbec9e
Riccardo Campani <riccardocampani96@gmail.com>
2023-07-13 22:07:00 +0200

    docs(nav.component): add JSDocs documentation for the NavComponent

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,7 +1,3 @@
-/**
- * NavComponent is an Angular component that represents a navigation menu.
- * It displays a menu with items retrieved from the MenuService.
- */
 import {Component, inject} from '@angular/core';
 import {MenuItem} from "primeng/api";
 import {MenuService} from "../../shared/services/menu.service";
@@ -9,6 +5,12 @@ import {ButtonModule} from "primeng/button";
 import {SlideMenuModule} from "primeng/slidemenu";
 import {LangChangeEvent, TranslateService} from "@ngx-translate/core";
 
+/**
+ * The NavComponent displays the navigation menu for the application.
+ * It uses PrimeNG's SlideMenu component to display the menu items.
+ *
+ * @class NavComponent
+ */
 @Component({
   selector: '[smdn-nav]',
   standalone: true,
@@ -18,18 +20,42 @@ import {LangChangeEvent, TranslateService} from "@ngx-translate/core";
 })
 export class NavComponent {
 
+  /**
+   * The MenuService instance used to retrieve the menu items.
+   *
+   * @private
+   * @type {MenuService}
+   * @memberof NavComponent
+   */
   private menuService = inject(MenuService);
+
+  /**
+   * The TranslateService instance used to translate the menu items.
+   *
+   * @private
+   * @type {TranslateService}
+   * @memberof NavComponent
+   */
   private translateService = inject(TranslateService);
 
   /**
-   * menuItems represents the menu items to be displayed in the navigation menu.
+   * The menu items to be displayed in the navigation menu.
+   *
+   * @public
+   * @type {MenuItem[]}
+   * @memberof NavComponent
    */
   public menuItems: MenuItem[] = [];
 
   /**
    * Creates an instance of NavComponent.
+   * @memberof NavComponent
    */
   constructor() {
+    /**
+     * Subscribes to the onLangChange event of the TranslateService to update the menu items
+     * when the language is changed.
+     */
     this.translateService.onLangChange.subscribe({
       next: (evt: LangChangeEvent) => {
         this.menuService.setTranslation(evt.translations.menu);
