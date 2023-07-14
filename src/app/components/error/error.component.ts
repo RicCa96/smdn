a9c6904024c57588a033ebb19e24b11a4ce1264e
Riccardo Campani <riccardocampani96@gmail.com>
2023-07-14 15:49:55 +0200

    docs(error.component): add JSDocs documentation for the ErrorComponent and set up i18n for it

--- src/app/components/error/error.component.ts
+++ src/app/components/error/error.component.ts
@@ -1,15 +1,32 @@
 import {Component} from '@angular/core';
 import {DynamicDialogConfig, DynamicDialogRef} from 'primeng/dynamicdialog';
 import {ButtonModule} from 'primeng/button';
+import {TranslateModule} from "@ngx-translate/core";
 
+/**
+ * The ErrorComponent displays an error message in a dialog box.
+ * It is used to inform the user of an error that has occurred in the application.
+ *
+ * @example
+ * <app-error></app-error>
+ *
+ * @export
+ * @class ErrorComponent
+ */
 @Component({
   standalone: true,
-  imports: [ButtonModule],
+  imports: [ButtonModule, TranslateModule],
   templateUrl: './error.component.html',
   styleUrls: ['./error.component.less']
 })
 export class ErrorComponent {
 
+  /**
+   * Creates an instance of ErrorComponent.
+   * @param {DynamicDialogRef} ref - The reference to the dialog box.
+   * @param {DynamicDialogConfig} config - The configuration for the dialog box.
+   * @memberof ErrorComponent
+   */
   constructor(
     public ref: DynamicDialogRef,
     public config: DynamicDialogConfig
