36c10a0518c742b6e1b48af1837e9277f77a4e9a
Riccardo Campani <riccardocampani96@gmail.com>
2023-07-14 16:16:05 +0200

    docs(auth.interceptor): add JSDocs for the AuthInterceptor

--- src/app/middleware/auth.interceptor.ts
+++ src/app/middleware/auth.interceptor.ts
@@ -2,12 +2,23 @@ import {HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {AuthService} from '../services/auth.service';
 
+/**
+ * An Angular HttpInterceptor that adds an Authorization header with a Bearer token to
+ * outgoing HTTP requests.
+ * The token is obtained from the AuthService.
+ */
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
   constructor(private authService: AuthService) {
   }
 
+  /**
+   * Intercepts an outgoing HTTP request and adds an Authorization header with a Bearer token.
+   * @param req - The outgoing HTTP request.
+   * @param next - The next HttpHandler in the chain.
+   * @returns An Observable of the HTTP response.
+   */
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     const authToken = this.authService.getToken();
     const authRequest = req.clone({
