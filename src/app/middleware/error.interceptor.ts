fc2567a242a8a47c71d5a2eef188d8e51ec710f6
Riccardo Campani <riccardocampani96@gmail.com>
2023-07-14 16:18:06 +0200

    docs(error.interceptor): add JSDocs for the ErrorInterceptor

--- src/app/middleware/error.interceptor.ts
+++ src/app/middleware/error.interceptor.ts
@@ -1,18 +1,33 @@
 import {ErrorComponent} from '../components/error/error.component';
-import {HttpErrorResponse, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {catchError} from 'rxjs/operators';
-import {throwError} from "rxjs";
+import {Observable, throwError} from "rxjs";
 import {DialogService} from "primeng/dynamicdialog";
 
+/**
+ * Interceptor that catches HTTP errors and displays them in a dialog box.
+ * @class
+ */
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
+  /**
+   * Creates an instance of ErrorInterceptor.
+   * @param {DialogService} dialogService - The service used to display the error dialog box.
+   */
   constructor(
     private dialogService: DialogService,
-  ) { }
+  ) {
+  }
 
-  intercept(req: HttpRequest<any>, next: HttpHandler) {
+  /**
+   * Intercepts HTTP requests and handles errors.
+   * @param {HttpRequest<any>} req - The HTTP request to handle.
+   * @param {HttpHandler} next - The next handler in the chain.
+   * @returns {Observable<HttpEvent<any>>} An observable that emits the HTTP response or an error.
+   */
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next
       .handle(req)
       .pipe(
