73962ea8d42e0ebe1c33e10501087cd4c9c4287a
Riccardo Campani <riccardocampani96@gmail.com>
2024-04-03 20:43:46 +0200

    chore(home): Add Italian translations for home page
    Several changes were made in the code to enable Italian translations for the home page. Text content in `it.json` were expanded with Italian translations for page titles, subtitles and other elements. Also, code in `home.component.ts` and `home.component.html` was updated to use the added translations instead of hardcoded text.

--- src/app/pages/public/home/home.component.ts
+++ src/app/pages/public/home/home.component.ts
@@ -1,18 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {TranslateModule} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.less'],
   standalone: true,
-  imports: [RouterLink]
+  imports: [RouterLink, TranslateModule]
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor() { }
+  get translatePrefix(): string {
+    return 'pages.public.home.';
+  }
 
-  ngOnInit() {
+  constructor() {
   }
 
 }
